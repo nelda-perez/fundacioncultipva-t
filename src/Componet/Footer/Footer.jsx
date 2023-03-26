98c3d248055525a6e68eedb8510afe425ae323cb
Noith <nimoba101@gmail.com>
2023-03-26 17:49:41 -0500

    Arreglando identacion del footer

--- src/Componet/Footer/Footer.jsx
+++ src/Componet/Footer/Footer.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import { infoFooter, mediaicons } from "./constant";
 import "./Style/footer.css";
 
-function footer(){
- return (<footer>{infoFooter.map((item,index) => (
-  <div className="Container" key={index}>
-    <span className="one">{item.icon} 
-       <a href={item.link}>{item.content}</a>
-    </span>
-  </div>
-))}
-<div className="containerIcon">
-{mediaicons.map((item,index)=>(
-  <div key={index}>
-    <a href={item.link} target="_blank">{item.icon}</a>
-  </div>
-))}
-</div>
-</footer>)
+function footer() {
+  return (
+    <footer>
+      {infoFooter.map((item, index) => (
+        <div className="Container" key={index}>
+          <span className="one">
+            {item.icon}
+            <a href={item.link}>{item.content}</a>
+          </span>
+        </div>
+      ))}
+      <div className="containerIcon">
+        {mediaicons.map((item, index) => (
+          <div key={index}>
+            <a href={item.link} target="_blank">
+              {item.icon}
+            </a>
+          </div>
+        ))}
+      </div>
+    </footer>
+  );
 }
 export default footer;
