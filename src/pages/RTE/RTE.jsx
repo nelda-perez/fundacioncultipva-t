d10d5595377ee0630205ed926453cad4d831e9ac
Noith <nimoba101@gmail.com>
2023-03-26 17:48:32 -0500

    Agregando el pdf del RTE y poniendolo en el componente RTE

--- src/pages/RTE/RTE.jsx
+++ src/pages/RTE/RTE.jsx
@@ -1,4 +1,17 @@
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import PDF from "../../assets/rte.pdf";
+
 function RTE() {
-  return <div>RTE</div>;
+  return (
+    <>
+      <h1>Descargar Documentos</h1>
+      <a href={PDF}>
+        <span>
+          <PictureAsPdfIcon sx={{ fontSize: "60px", color: "red" }} />
+        </span>
+        <span style={{ color: "black" }}>RTE</span>
+      </a>
+    </>
+  );
 }
 export default RTE;
