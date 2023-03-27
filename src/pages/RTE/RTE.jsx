64f6ab5d12e66f89b74f1c03c49bc19329bd8038
Noith <nimoba101@gmail.com>
2023-03-26 19:24:59 -0500

    Modificando y dando por terminado el componente RTE, por el momento, falta el Responsive

--- src/pages/RTE/RTE.jsx
+++ src/pages/RTE/RTE.jsx
@@ -1,17 +1,18 @@
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import PDF from "../../assets/rte.pdf";
+import "./rte.css";
 
 function RTE() {
   return (
-    <>
-      <h1>Descargar Documentos</h1>
+    <div className="container-rte">
+      <h1 className="team-title">Descargar Documentos</h1>
       <a href={PDF}>
         <span>
           <PictureAsPdfIcon sx={{ fontSize: "60px", color: "red" }} />
         </span>
         <span style={{ color: "black" }}>RTE</span>
       </a>
-    </>
+    </div>
   );
 }
 export default RTE;
