ff1c4f9428dd7bfe143f8e7fb7c6b6e95a7d3912
Noith <nimoba101@gmail.com>
2023-07-28 16:46:24 -0500

    style: add new design to whatsapp logo

--- src/components/Iconwp/Iconwp.jsx
+++ src/components/Iconwp/Iconwp.jsx
@@ -1,12 +1,13 @@
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import style from "./btn-whatsapp.module.css";
+import whatsAppImg from "../../assets/icon-whatsapp.svg";
 
 function Iconwp() {
   return (
     <a
       href="https://wa.me/+573132112921"
       target="_blank"
-      className="container-whatsapp__icon">
-      <WhatsAppIcon sx={{ fontSize: "60px" }} />
+      className={style.btnWhatsapp}>
+      <img src={whatsAppImg} alt="Contactar por WhatsApp" />
     </a>
   );
 }
