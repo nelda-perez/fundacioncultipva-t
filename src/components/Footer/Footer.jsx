46aeaeb41d914b3d51dc9b0bfae20c93f258903d
Noith <nimoba101@gmail.com>
2023-08-14 15:41:56 -0500

    feat: modify some issues

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,32 +10,22 @@ function footer() {
     <footer>
         <div className="wave"></div>
       <section className="containerRows">
-        <div className="containerTerms">
-          <figure className="footerLogo">
-            <img src={logo} alt="logo" />
-          </figure>
-          <a href={terms} target="_blank">Tratamiento de datos</a>
-        </div>
         <div className="container container-link">
           <div className="containerItems">
-            <ItemFooter
-              icon="address"
-              link="https://goo.gl/maps/ncJVDW3rP3PoLWsL7"
-              content="Kra.76a Sur No.62 K 48"
-            />
             <ItemFooter
               icon="phone"
               link="https://wa.me/+573132112921"
               content="3132112921"
             />
-          </div>
-          <div className="containerItems">
             <ItemFooter
               icon="email"
               link="mailto:contactenos@fundacioncultivar.org"
               content="contactenos@fundacioncultivar.org"
             />
+          </div>
+          <div className="containerItems">
             <ItemFooter icon="info" link="/rte" content="Transparencia" />
+            <a href={terms} target="_blank">Tratamiento de datos</a>
           </div>
         </div>
         <div className="containerIcon">
--- src/pages/RTE/components/ItemPdf/ItemPdf.jsx
+++ src/pages/RTE/components/ItemPdf/ItemPdf.jsx
@@ -2,11 +2,13 @@ import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import { FILESPDF } from "../../constant";
 import RTE2021 from "../../../../assets/rte.pdf";
 import RTE2022 from "../../../../assets/rte2022.pdf";
+import RUT2023 from "../../../../assets/Rut_Actual_2023.pdf"
 
 function ItemPdf() {
   const File = {
     rte2021: RTE2021,
     rte2022: RTE2022,
+    rut2023: RUT2023
   };
   return (
     <>
